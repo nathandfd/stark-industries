6d6bf3304cf6bf1f2c9f18f090cdaeb598a98f2e
Lucas <lucascassan@outlook.com>
2021-05-05 16:43:16 +0200

    Update Lucas

--- src/Controller/SalesmanController.php
+++ src/Controller/SalesmanController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Contract;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\NewContratRequestFormType;
+
 /**
  * @Route("/salesman")
  */
@@ -23,7 +28,41 @@ class SalesmanController extends AbstractController
     /**
      * @Route("/new-contract", name="new-contract")
      */
-    public function newContract(){
-        return $this->render('salesman/new-contract.html.twig');
+    public function newContract(Request $request){
+		$contrat = new Contract();
+		$form=$this->createForm(NewContratRequestFormType::class,$contrat);
+		$form->handleRequest($request);
+		if($form->isSubmitted() && $form->isValid()){
+			$em = $this->getDoctrine()->getManager();
+
+			$contrat->setSalesman($this->getUser());
+			$contrat->setStatus(0);
+
+			$infoClient = [
+				'gender'=>$form->get('gender')->getData(),
+				'lastname'=>$form->get('lastname')->getData(),
+			];
+			$contrat->setInfoClient($infoClient);
+
+			$infoPrelevement = [
+				'gender'=>$form->get('gender')->getData(),
+				'lastname'=>$form->get('lastname')->getData(),
+			];
+			$contrat->setInfoPrelevement($infoPrelevement);
+			$contrat->setNumeroVerif(1234);
+
+			$em->persist($contrat);
+			$em->flush();
+			$em->refresh($contrat);
+
+			$num_contrat = $this->getUser()->getMatricule();
+			$num_contrat .= str_pad($contrat->getId(), 4, 0, STR_PAD_LEFT);;
+			$contrat->setNumContrat($num_contrat);
+			$em->flush();
+			dd($contrat);
+
+		}
+
+		return $this->render('salesman/new-contract.html.twig', ['form' => $form->createView()]);
     }
 }
---
+++ src/Form/NewContratRequestFormType.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contract;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewContratRequestFormType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+
+		$builder
+
+			//ADHÉRENT
+
+			->add('gender', ChoiceType::class,[
+				'label'=>'Genre',
+				'mapped'=>false,
+				'expanded'=>true,
+				'multiple'=>false,
+				'choices'=>[
+					'M.'=>'m',
+					'Mme'=>'f'
+				]
+			])
+			->add('lastname', TextType::class,[
+				'label'=>'Nom',
+				'mapped'=>false,
+			])
+			->add('firstname', TextType::class,[
+				'label'=>'Prénom',
+				'mapped'=>false,
+			])
+			->add('birthday', BirthdayType::class,[
+				'label'=>'Date de naissance',
+				'mapped'=>false,
+				'placeholder' => [
+					'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+				]
+			])
+			->add('address', TextType::class,[
+				'label'=>'Adresse',
+				'mapped'=>false,
+			])
+			->add('zipcode', TextType::class,[
+				'label'=>'Code postal',
+				'mapped'=>false,
+			])
+			->add('city', TextType::class,[
+				'label'=>'Ville',
+				'mapped'=>false,
+			])
+			->add('country', TextType::class,[
+				'label'=>'Pays',
+				'mapped'=>false,
+			])
+
+			->add('phone', TelType::class,[
+				'label'=>'Téléphone fixe',
+				'mapped'=>false,
+			])
+			->add('mobile', TelType::class,[
+				'label'=>'Téléphone mobile',
+				'mapped'=>false,
+			])
+			->add('mail', TextType::class,[
+				'label'=>'Adresse mail',
+				'mapped'=>false,
+			])
+
+			//MANDAT DE PRÉLÉVEMENT
+
+			->add('iban', TextType::class,[
+				'label'=>'IBAN',
+				'mapped'=>false,
+
+			])
+			->add('bic', TextType::class,[
+				'label'=>'BIC',
+				'mapped'=>false,
+
+			])
+			->add('save', SubmitType::class,[
+				'label'=>'Valider'
+			]);
+		}
+
+}
