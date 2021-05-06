16c18ef96e37e3434a880a31904c6c7b645939a8
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-05-06 12:06:45 +0200

    Redirection on access denied

---
+++ src/Security/AccessDeniedListener.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Security;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class AccessDeniedListener implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
+{
+    private $url;
+
+    public function __construct(UrlGeneratorInterface $url){
+        $this->url = $url;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return[
+          KernelEvents::EXCEPTION => [
+              'onKernelException',
+              2
+          ]
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event):void
+    {
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof AccessDeniedException){
+            return;
+        }
+
+        $event->setResponse(new RedirectResponse($this->url->generate('app_login')));
+    }
+}
