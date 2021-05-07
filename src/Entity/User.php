79948224d1a74b46e160780ab999f5a7b8a61183
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-05-07 11:00:14 +0200

    Let's go prod

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -26,7 +26,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="array")
      */
     private $roles = ["ROLE_SALESMAN"];
 
