458addd6db431c9adb0ceb573c74b26c94508ff0
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-05-12 13:23:34 +0200

    Cancellation mail done

--- src/Controller/BackofficeController.php
+++ src/Controller/BackofficeController.php
@@ -10,10 +10,14 @@ use Knp\Bundle\SnappyBundle\KnpSnappyBundle;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Knp\Snappy\Pdf;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Message;
 use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/backoffice")
@@ -45,12 +49,25 @@ class BackofficeController extends AbstractController
     public function updateStatus(
         $contratid,
         $newstatus,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer
     ): JsonResponse {
 
         $contract = $entityManager->getRepository(Contract::class)->find($contratid);
         $contract->setStatus($newstatus);
         $entityManager->flush();
+        if ($newstatus == 4){
+            $email = (new TemplatedEmail())
+                ->from(new Address('contact@groupe-stark-industries.fr', 'Stark industries'))
+                ->to($contract->getInfoClient()['mail'])
+                ->subject('Confirmation de résiliation chez Stark industries')
+                ->htmlTemplate('mail_template/cancellation_mail.html.twig')
+                ->context([
+                    'name'=> $contract->getInfoClient()['firstname']
+                ]);
+
+            $mailer->send($email);
+        }
         return new JsonResponse(true);
     }
 
@@ -60,14 +77,6 @@ class BackofficeController extends AbstractController
     public function exportPdf(EntityManagerInterface $entityManager,Request $request, Pdf $pdf): Response
     {
         $contract = $entityManager->getRepository(Contract::class)->find($request->query->get('contratid'));
-
-//        return $this->render(
-//            'backoffice/export.html.twig',
-//            array(
-//                'controller_name' => 'BackofficeController',
-//                'contrat' => $contract
-//            )
-//        );
 
         $pdf->setBinary("\"../src/Wkhtmltopdf/bin/wkhtmltopdf.exe\"");
         $pdf->setTemporaryFolder("../var/cache");
