9141d0224d39c995939e9ea0861e56f61466bbf4
Nathan <nathan.dufaud@gmail.com>
2021-05-21 19:43:07 +0200

    Update

--- src/Controller/BackofficeController.php
+++ src/Controller/BackofficeController.php
@@ -166,6 +166,83 @@ class BackofficeController extends AbstractController
         return $this->file($filename);
     }
 
+    /**
+     * @Route("/export-all-csv", name="backoffice_export_all_csv")
+     */
+    public function exportAllCsv(EntityManagerInterface $entityManager,Request $request)
+    {
+        $contracts = $entityManager->getRepository(Contract::class)->findAll();
+        $tempFolder = "../var/cache";
+        $filename = $tempFolder."/contrats_export.csv";
+        if (file_exists($filename)){
+            unlink($filename);
+        }
+        if (0) {
+            exit("Impossible d'ouvrir le fichier <$filename>\n");
+        }
+        $csvFile = fopen($filename,'w');
+
+        $header = [
+            'Numéro de contrat',
+            'Commercial',
+            'Distributeur',
+            'Client',
+            'Adresse',
+            'Code postal',
+            'Ville',
+            'Numéro de téléphone',
+            'Mail',
+            'Date de signature',
+            'Status du contrat',
+            'RIB',
+            'BIC'
+        ];
+
+        foreach ($contracts as $key=>$contract){
+            switch ($contract->getStatus()){
+                case '1':
+                    $status = 'En attente client';
+                    break;
+                case '2':
+                    $status = 'En attente back-office';
+                    break;
+                case '3':
+                    $status = 'Validé';
+                    break;
+                case '4':
+                    $status = 'Rétracté';
+                    break;
+                case '5':
+                    $status = 'Injoignable';
+                    break;
+                case '6':
+                    $status = 'Impayé';
+                    break;
+                default:
+                    $status = 'Erreur';
+                    break;
+            }
+            $data = [
+                $contract->getNumContrat(),
+                $contract->getSalesman()->getFirstname().' '.$contract->getSalesman()->getName(),
+                $contract->getSalesman()->getDistributor()->getName(),
+                (($contract->getInfoClient()['gender'] == 'm')?'M.':'Mme').' '.$contract->getInfoClient()['firstname'].' '.$contract->getInfoClient()['lastname'],
+                $contract->getInfoClient()['address'],
+                $contract->getInfoClient()['zipcode'],
+                $contract->getInfoClient()['city'],
+                $contract->getInfoClient()['mobile'],
+                $contract->getInfoClient()['mail'],
+                $contract->getCreated(),
+                $status,
+                $contract->getInfoPrelevement()['iban'],
+                $contract->getInfoPrelevement()['bic'],
+            ];
+            fputcsv($csvFile,$data);
+        }
+        fclose($csvFile);
+        return $this->file($filename);
+    }
+
     private function removeDir($dir) {
         if (is_dir($dir)) {
             $objects = scandir($dir);
