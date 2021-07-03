f28d9a5a71c183ce6a9f18dc7c971f6c41eed057
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-07-03 19:53:33 +0200

    update

--- docker/php-apache/configure-symfony.sh
+++ docker/php-apache/configure-symfony.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 /composer/composer.phar install
 npm install
+npm install -D tailwindcss@latest postcss@latest autoprefixer@latest
 npm run build
 php bin/console d:s:u -f
 php bin/console c:c
